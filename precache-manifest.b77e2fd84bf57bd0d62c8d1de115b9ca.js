9f90ab2cfccceb5538f55aec90e73ccde24ef80f
webDevSaurav <webdevelopersaurav@gmail.com>
2020-05-22 16:34:34 +0530

    Updates

---
+++ precache-manifest.b77e2fd84bf57bd0d62c8d1de115b9ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae23116b64e288a6ebc22b022703d50d",
+    "url": "/robofriends-andrei-react/index.html"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends-andrei-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f5a9addb586f9afcbcdb",
+    "url": "/robofriends-andrei-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a100be31e3a0675486cf",
+    "url": "/robofriends-andrei-react/static/js/2.ca4a7502.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-andrei-react/static/js/2.ca4a7502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a9addb586f9afcbcdb",
+    "url": "/robofriends-andrei-react/static/js/main.e1521ab1.chunk.js"
+  },
+  {
+    "revision": "f6cd28abe7df7af6cd28",
+    "url": "/robofriends-andrei-react/static/js/runtime-main.8403ba40.js"
+  }
+]);
